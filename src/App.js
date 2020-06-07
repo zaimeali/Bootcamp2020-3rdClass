88be3b7c70c720666da6f45a5b496d2a76eed934
Zaime Ali <zaimeali1997@gmail.com>
2020-06-07 21:19:53 +0500

    Sending more than one props

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App(props) {
   return (
     <div>
       <div><h2>Third Class</h2></div>
+      <hr/>
       <div>Hello World from REACT App</div>
       <div>Written in App.js File</div>
+      <hr/>
       <div>Code by: {props.name}</div>
+      <div>My age is: {props.age}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import App from './App';
 ReactDOM.render(
   // React.createElement('h1', null, "h1 tag created by React.createElement"),
   // React.createElement('h1', null, React.createElement('span', null, "Span nested in h1")),
-  <App name="Abdul Raffay Rizwan" />,
+  <App name="Abdul Raffay Rizwan" age="23" />,
   document.getElementById('root')
 );
