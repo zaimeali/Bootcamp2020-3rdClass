916ec95d91e15202760911f399601722d2852a57
Zaime Ali <zaimeali1997@gmail.com>
2020-06-07 21:17:26 +0500

    Sending Data through props into the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div>
       <div><h2>Third Class</h2></div>
       <div>Hello World from REACT App</div>
       <div>Written in App.js File</div>
+      <div>Code by: {props.name}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import App from './App';
 ReactDOM.render(
   // React.createElement('h1', null, "h1 tag created by React.createElement"),
   // React.createElement('h1', null, React.createElement('span', null, "Span nested in h1")),
-  <App />,
+  <App name="Abdul Raffay Rizwan" />,
   document.getElementById('root')
 );
